fc7f2618dfa8b2228b569a5004aa414cf75c4b9e
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2024-11-05 21:35:24 -0500

    feat(location): implement google map places autocomplete

--- app/components/InputText.tsx
+++ app/components/InputText.tsx
@@ -2,15 +2,24 @@ import { forwardRef } from 'react';
 
 type InputTextProps = {
   fieldAttributes: React.InputHTMLAttributes<HTMLInputElement>;
+  value?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
 };
 
-const InputText = forwardRef<HTMLInputElement, InputTextProps>(({ fieldAttributes }, ref) => (
-  <input
-    {...fieldAttributes}
-    ref={ref}
-    key={null}
-    className="block w-full rounded-md bg-transparent border-0 py-1.5 pl-3 text-on-surface shadow-sm ring-1 ring-inset ring-around-surface focus:outline-none  focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6 aria-[invalid]:ring-error"
-  />
-));
+const InputText = forwardRef<HTMLInputElement, InputTextProps>(
+  ({ fieldAttributes, onChange, value, placeholder }, ref) => (
+    <input
+      {...fieldAttributes}
+      onChange={onChange}
+      value={value}
+      placeholder={placeholder}
+      ref={ref}
+      key={null}
+      autoComplete="off"
+      className="block w-full rounded-md bg-transparent border-0 py-1.5 pl-3 pr-2 text-on-surface shadow-sm ring-1 ring-inset ring-around-surface focus:outline-none  focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6 aria-[invalid]:ring-error"
+    />
+  )
+);
 
 export default InputText;
--- app/routes/_user.tsx
+++ app/routes/_user.tsx
@@ -1,11 +1,33 @@
 import { LoaderFunctionArgs } from '@remix-run/node';
 import { Outlet } from '@remix-run/react';
+import { ExternalScriptsFunction } from 'remix-utils/external-scripts';
 import { Breadcrumb, Header } from '~/components';
+
+export async function loader() {
+  return {
+    googleApiKey: process.env.GOOGLE_API_KEY,
+  };
+}
+
+type LoaderData = Awaited<ReturnType<typeof loader>>;
+
+const scripts: ExternalScriptsFunction<LoaderData> = ({ data }) => {
+  const googleApiKey = data.googleApiKey;
+  if (!googleApiKey) return [];
+
+  return [
+    {
+      src: `https://maps.googleapis.com/maps/api/js?key=${googleApiKey}&libraries=places`,
+      async: true,
+    },
+  ];
+};
 
 export const handle = {
   breadcrumb: ({ params }: LoaderFunctionArgs) => {
     return <Breadcrumb name={params.username} to={params.username} />;
   },
+  scripts,
 };
 
 export default function UserLayoutRoute() {
